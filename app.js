40ad44668873f8bca0f9437751d01a25debf0171
sc22dm <sc22dm@fsu.edu>
2026-05-28 13:46:06 -0400

    adding code

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-//this is the app
+// FAKE CHAT BOX RESPONSE
+function sendChat() {
+    let input = document.getElementById("chatInput").value;
+    let response = document.getElementById("chatResponse");
+
+    response.innerText = "Searching for: " + input;
+}
+
+// SIMPLE AUTOCOMPLETE MOCK
+let doctors = ["Cardiologist", "Dermatologist", "Pediatrics", "Neurology"];
+
+document.getElementById("doctorSearch").addEventListener("input", function () {
+    let list = document.getElementById("suggestions");
+    list.innerHTML = "";
+
+    doctors.forEach(doc => {
+        if (doc.toLowerCase().includes(this.value.toLowerCase())) {
+            let li = document.createElement("li");
+            li.innerText = doc;
+            list.appendChild(li);
+        }
+    });
+});
